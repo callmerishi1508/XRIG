f7ff886c5ec282c247c2862de811f50c417241e2
callmerishi1508 <131880796+callmerishi1508@users.noreply.github.com>
2023-10-16 21:08:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask,redirect,url_for,render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
+@app.route("/<name>")
+def home(name):
     return render_template("index.html",content=name,r=2)
     
 if __name__ == "__main__":
