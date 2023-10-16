514147dd9dbe05c39860fa5a5a9e78962ac4a6e0
callmerishi1508 <131880796+callmerishi1508@users.noreply.github.com>
2023-10-16 21:01:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,redirect,url_for,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return render_template("index.html",content=name,r=2)
     
 if __name__ == "__main__":
     app.run(debug=True)
